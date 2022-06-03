e56d86b0bf93a1a4a67d977e1c72e37d6debc23d
Ryan Harmon <heyharmon@gmail.com>
2022-06-03 09:45:22 -0600

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup>
-import { useRoute } from 'vue-router'
-import AppNavigation from '@/app/components/AppNavigation.vue'
-
-const route = useRoute()
-</script>
-
 <template>
   <div v-if="route.params.organization">
     <AppNavigation />
     <router-view />
   </div>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import AppNavigation from '@/app/components/AppNavigation.vue'
+
+const route = useRoute()
+</script>
 
 <style lang="scss">
 @import '@/app/styles/styles.scss';
