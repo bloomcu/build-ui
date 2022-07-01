da516367eb6031310d92bf7e52e1c86203bdfb38
Ryan Harmon <heyharmon@gmail.com>
2022-07-01 11:54:24 -0600

    Update Assets.vue

--- src/views/assets/Assets.vue
+++ src/views/assets/Assets.vue
@@ -1,22 +1,15 @@
 <template>
   <LayoutDefault>
-    <div class="padding-y-md">
-      <h3>Assets</h3>
-    </div>
-    
-    <div class="padding-y-md">
-      <div v-for="group in groups" :key="group.name" class="grid gap-sm margin-bottom-xl padding-y-sm border-top border-contrast-lower">
-        <div class="col-4 padding-right-md">
-          <h4 class="margin-bottom-sm">{{ group.title }}</h4>
-          <p class="text-sm margin-bottom-sm" v-html="group.description"></p>
-          <AppTodoList :todos="group.todos" />
-        </div>
-        <div class="col-8 bg-dark radius-md padding-sm">
-          <FileUploader folder="bloomcu" :group="group.name"/>
-          <FileList @selected="openModal" @destroyed="destroyFile" :files="fileStore.filterByGroup(group.name)"/>
-        </div>
+      <div class="flex container border-top max-width-xxl">
+        <aside class="position-relative z-index-1 width-100% border-right max-width-xxxxs padding-y-sm padding-x-md">
+          <AppNestedMenu :items="groups"/>
+        </aside>
+        
+        <main class="position-relative z-index-1 flex-grow height-auto padding-y-md padding-x-lg">
+          <FileUploader :folder="route.params.organization" :group="groups[toggled].name"/>
+          <FileList @selected="openModal" @destroyed="destroyFile" :files="fileStore.filterByGroup(groups[toggled].name)"/>
+        </main>
       </div>
-    </div>
     
     <FileModal @closed="closeModal" @destroyed="destroyFile" :file="modalData" :class="modalData ? 'modal--is-visible' : ''"/>
   </LayoutDefault>
@@ -24,17 +17,25 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { useFileStore } from '@/domain/files/store/useFileStore'
 
+// Router
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+// Stores
+import { useFileStore } from '@/domain/files/store/useFileStore'
+const fileStore = useFileStore()
+
+// Components
 import LayoutDefault from '@/app/layouts/LayoutDefault.vue'
+import AppNestedMenu from '@/app/components/nested-menu/AppNestedMenu.vue'
 import FileUploader from '@/domain/files/components/FileUploader.vue'
 import FileList from '@/domain/files/components/FileList.vue'
 import FileModal from '@/domain/files/components/file-modal/FileModal.vue'
-import AppTodoList from '@/app/components/AppTodoList.vue'
 
-const route = useRoute()
-const fileStore = useFileStore()
+// Composables
+import useToggle from '@/app/composables/useToggle.js'
+const { toggle, toggled } = useToggle()
 
 onMounted(() => {
   fileStore.index()
@@ -60,70 +61,34 @@ function destroyFile(file) {
 
 const groups = [
   {
+    id: 0,
     name: 'brand',
     title: 'Brand Guide',
-    description: 'Upload your brand usage guide document(s) here.',
-    todos: [
-      {
-        title: 'Format: PDF',
-        formats: ['pdf'],
-      }
-    ]
   },
   {
+    id: 1,
     name: 'logo',
     title: 'Logo',
-    description: 'Upload your logo(s) here.',
-    todos: [
-      {
-        title: 'Format: SVG and EPS',
-        formats: ['svg', 'eps'],
-      }
-    ]
   },
-  // {
-  //   name: 'color',
-  //   title: 'Color Palette',
-  //   description: 'Upload an image or document containing your color palette.',
-  // },
   {
+    id: 2,
     name: 'desktop-fonts',
     title: 'Desktop Fonts',
-    description: 'Upload your fonts for desktop usage.',
-    todos: [
-      {
-        title: 'Format: TTF, TTC or OTF',
-        formats: ['ttf', 'ttc', 'otf'],
-      }
-    ]
   },
   {
+    id: 3,
     name: 'web-fonts',
     title: 'Web Fonts',
-    description: 'Upload your fonts for web usage.',
-    todos: [
-      {
-        title: 'Format: TTF, OTF, WOFF, WOFF2 or EOT',
-        formats: ['ttf', 'otf', 'woff', 'woff2', 'eot'],
-      }
-    ]
   },
   {
+    id: 4,
     name: 'photos',
     title: 'Photography',
-    description: 'Upload your photography.',
-    todos: [
-      {
-        title: 'Format: JPG, JPEG or PNG',
-        formats: ['jpg', 'jpeg', 'png'],
-      }
-    ]
   },
   {
+    id: 5,
     name: 'other',
     title: 'Other Media',
-    description: 'Upload other brand media such as patterns, icons or other graphics.',
-    todos: []
   },
 ]
 </script>
