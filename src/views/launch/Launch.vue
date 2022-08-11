3cdf18cd528fc7fe4a2ece1b63124b9343414b8f
Ryan Harmon <heyharmon@gmail.com>
2022-08-11 15:14:59 -0600

    Reorganize launch components

--- src/views/launch/Launch.vue
+++ src/views/launch/Launch.vue
@@ -9,22 +9,20 @@
         <LaunchStep4/>
         <LaunchStep5/>
       </main>
-      
       <aside class="position-relative z-index-1 width-100% border-left max-width-xxxs padding-y-md padding-x-md">
         <LaunchContacts/>
       </aside>
-      
     </div>
   </LayoutWithoutNavigation>
 </template>
 
 <script setup>
 import LayoutWithoutNavigation from '@/app/layouts/LayoutWithoutNavigation.vue'
-import LaunchHero from '@/views/launch/steps/LaunchHero.vue'
-import LaunchContacts from '@/views/launch/steps/LaunchContacts.vue'
-import LaunchStep1 from '@/views/launch/steps/LaunchStep1.vue'
-import LaunchStep2 from '@/views/launch/steps/LaunchStep2.vue'
-import LaunchStep3 from '@/views/launch/steps/LaunchStep3.vue'
-import LaunchStep4 from '@/views/launch/steps/LaunchStep4.vue'
-import LaunchStep5 from '@/views/launch/steps/LaunchStep5.vue'
+import LaunchHero from '@/views/launch/components/LaunchHero.vue'
+import LaunchContacts from '@/views/launch/components/LaunchContacts.vue'
+import LaunchStep1 from '@/views/launch/components/steps/LaunchStep1.vue'
+import LaunchStep2 from '@/views/launch/components/steps/LaunchStep2.vue'
+import LaunchStep3 from '@/views/launch/components/steps/LaunchStep3.vue'
+import LaunchStep4 from '@/views/launch/components/steps/LaunchStep4.vue'
+import LaunchStep5 from '@/views/launch/components/steps/LaunchStep5.vue'
 </script>
