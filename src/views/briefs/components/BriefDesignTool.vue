849a95eca7ee41ea1d9bf142a3a3f7b2e8044393
Ryan Harmon <heyharmon@gmail.com>
2022-08-30 16:21:53 -0600

    Update BriefDesignTool.vue

--- src/views/briefs/components/BriefDesignTool.vue
+++ src/views/briefs/components/BriefDesignTool.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="text-xs text-uppercase letter-spacing-lg color-primary">Step 1</p>
     <h1>Log into design tool</h1>
-    <p>We shared your URL and credentials on Upwork. Use that information now to get started.</p>
+    <p>We shared your URL and credentials. Use that information now to get started.</p>
     
     <div class="flex gap-xs margin-top-md padding-y-md border-top">
       <button @click="toggle('BriefVideo')" class="btn btn--subtle">Back</button>
