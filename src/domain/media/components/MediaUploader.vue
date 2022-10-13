5b245faeac8d2c054f1cec39930929812c2285da
Ryan Harmon <heyharmon@gmail.com>
2022-10-13 16:02:10 -0600

    Setup filepond max file size validation

--- src/domain/media/components/MediaUploader.vue
+++ src/domain/media/components/MediaUploader.vue
@@ -1,15 +1,69 @@
 <template>
-    <div>
-        <input type="file" name="uploader" id="uploader" ref="uploader" multiple>
-    </div>
+  <div>
+    <input type="file" name="uploader" id="uploader" ref="uploader" multiple>
+  </div>
 </template>
 
 <script setup>
 import axios from 'axios'
 import * as Filepond from 'filepond'
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
 
 import { ref, onMounted } from 'vue'
 import { useMediaStore } from '@/domain/media/store/useMediaStore'
+
+const uploader = ref() // Input binding
+const mediaStore = useMediaStore()
+
+Filepond.registerPlugin(FilePondPluginFileValidateSize)
+
+onMounted(() => {
+  const pond = Filepond.create(uploader.value, {
+    allowRevert: false,
+    allowFileSizeValidation: true,
+    maxFileSize: '100MB',
+
+    // Process files
+    server: {
+      process: (fieldName, file, metadata, load, error, progress, abort) => {
+        const cancelToken = axios.CancelToken.source()
+        
+        mediaStore.store(file, props.collection, props.tag, {
+          onUploadProgress: event => {
+            progress(e.lengthComputable, e.loaded, e.total)
+          },
+          cancelToken: cancelToken.token
+        })
+        .then((response) => {
+          load(file.name)
+        })
+        
+        return {
+          // Abort callback if canceling
+          abort: () => {
+            cancelToken.cancel()
+            abort()
+          }
+        }
+      }
+    },
+
+    // File is initally added
+    onaddfile: (error, file) => {
+      if (error) { return }
+    },
+
+    // File is finished uploading
+    onprocessfile: (error, file) => {
+      if (error) { return }
+
+      // Remove file from uploads list (delayed)
+      setTimeout(function () {
+        pond.removeFile(file)
+      }.bind(this), 2000)
+    }
+  })
+}) // End onMounted
 
 const props = defineProps({
   collection: {
@@ -20,57 +74,6 @@ const props = defineProps({
     type: String,
   },
 })
-
-const uploader = ref() // Input binding
-
-const mediaStore = useMediaStore()
-
-onMounted(() => {
-    const pond = Filepond.create(uploader.value, {
-        allowRevert: false,
-
-        // Process files
-        server: {
-            process: (fieldName, file, metadata, load, error, progress, abort) => {
-                const cancelToken = axios.CancelToken.source()
-                
-                mediaStore.store(file, props.collection, props.tag, {
-                  onUploadProgress: event => {
-                      progress(e.lengthComputable, e.loaded, e.total)
-                  },
-                  cancelToken: cancelToken.token
-                })
-                .then((response) => {
-                  load(file.name)
-                })
-                
-                return {
-                    // Abort callback if canceling
-                    abort: () => {
-                        cancelToken.cancel()
-                        abort()
-                    }
-                }
-                
-            }
-        },
-
-        // File is initally added
-        onaddfile: (error, file) => {
-            if (error) { return }
-        },
-
-        // File is finished uploading
-        onprocessfile: (error, file) => {
-            if (error) { return }
-
-            // Remove file from uploads list (delayed)
-            setTimeout(function () {
-                pond.removeFile(file)
-            }.bind(this), 2000)
-        }
-    })
-})
 </script>
 
 <style lang="scss">
@@ -78,27 +81,27 @@ onMounted(() => {
 
 .filepond--root,
 .filepond--drop-label label {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .filepond--panel-root {
-    border-radius: var(--radius-lg);
-    background: var(--color-bg-dark);
+  border-radius: var(--radius-lg);
+  background: var(--color-bg-dark);
 }
 
 .filepond--credits {
-    display: none;
+  display: none;
 }
 
 .filepond--item-panel {
-    background-color: var(--color-primary);
+  background-color: var(--color-primary);
 }
 
 [data-filepond-item-state='processing-complete'] .filepond--item-panel {
-    background-color: var(--color-success);
+  background-color: var(--color-success);
 }
 
 .filepond--file-action-button {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
