5c6eb7ed4a2e587c8424352c01f7d58a3826e50d
Ryan Harmon <heyharmon@gmail.com>
2022-11-13 14:53:14 -0700

    Highlight selected pages when hovering over any action

--- src/views/content/components/ContentTable.vue
+++ src/views/content/components/ContentTable.vue
@@ -68,6 +68,8 @@
             <ContentCategoryChip 
               :id="page.id" 
               :category="page.category" 
+              @mouseover="highlight(page.id)"
+              @mouseleave="unhighlight()"
               class="border-right padding-right-sm"
             />
             
@@ -81,7 +83,12 @@
             />
             
             <!-- Destroy -->
-            <button @click="destroy(page.id)" class="app-action-icon reset">
+            <button 
+              @click="destroy(page.id)" 
+              @mouseover="highlight(page.id)"
+              @mouseleave="unhighlight()"
+              class="app-action-icon reset"
+            >
               <svg class="icon" viewBox="0 0 24 24"><g stroke-linecap="square" stroke-miterlimit="10" fill="none" stroke="currentColor" stroke-linejoin="miter"><path d="M20,9l-.867,12.142A2,2,0,0,1,17.138,23H6.862a2,2,0,0,1-1.995-1.858L4,9"></path><line x1="1" y1="5" x2="23" y2="5"></line><path data-cap="butt" d="M8,5V1h8V5" stroke-linecap="butt"></path></g></svg>
             </button>
           </div><!-- End right -->
