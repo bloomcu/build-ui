b019fa32eb546b6e9de8f6c9654280d934c16222
Ryan Harmon <heyharmon@gmail.com>
2023-01-26 13:00:31 -0700

    Cleanup navigation

--- src/app/components/AppNavigation.vue
+++ src/app/components/AppNavigation.vue
@@ -81,22 +81,21 @@
           </li>
           
           <li class="f-header__item">
-            <router-link 
-              :to="{ name: 'styleDesignBrief', params: { organization: auth.organization }}" 
-              class="f-header__link"
-            >
-              Design Brief
-            </router-link>
+            <RouterLink :to="{ name: 'designs', params: { organization: auth.organization}}" class="f-header__link reset cursor-pointer">
+              <span>Design</span>
+              <svg class="f-header__dropdown-icon icon" height="12" width="12" viewBox="0 0 12 12"><g fill="#000000"><path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z" fill="#000000"></path></g></svg>
+            </RouterLink>
+            <ul class="f-header__dropdown">
+              <li>
+                <RouterLink :to="{ name: 'designs', params: { organization: auth.organization}}" class="f-header__dropdown-link">
+                  Style Concepts
+                </RouterLink>
+                <RouterLink :to="{ name: 'styleDesignBrief', params: { organization: auth.organization }}" class="f-header__dropdown-link">
+                  Designer Brief
+                </RouterLink>
+              </li>
+            </ul>
           </li>
-          
-          <!-- <li class="f-header__item">
-            <router-link 
-              :to="{ name: 'designs', params: { organization: auth.organization}}" 
-              class="f-header__link"
-            >
-              Designs
-            </router-link>
-          </li> -->
           
           <!-- <li class="f-header__item">
             <router-link 
@@ -151,9 +150,9 @@
         <!-- User -->
         <ul class="f-header__list">
           <li class="f-header__item margin-right-md">
-            <button class="f-header__link reset cursor-pointer">
+            <!-- <button class="f-header__link reset cursor-pointer">
               Help
-            </button>
+            </button> -->
           </li>
           
           <li class="f-header__item margin-right-md">
